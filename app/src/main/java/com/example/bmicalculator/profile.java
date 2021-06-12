2d8f47ad47a6f92b3877dfbbcea960a936153564
Amirah <2019311667@student.uitm.edu.my>
2021-06-12 15:25:13 +0800

    bmi calculator

--- app/src/main/java/com/example/bmicalculator/profile.java
+++ app/src/main/java/com/example/bmicalculator/profile.java
@@ -11,26 +11,26 @@ import android.text.method.LinkMovementMethod;
 import android.widget.TextView;
 
 public class profile extends AppCompatActivity {
-//    public Button button;
-//    TextView textView;
+    public Button button;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
 
-//        textView = findViewById(R.id.textView5);
-//        textView.setMovementMethod(LinkMovementMethod.getInstance());
+        textView = findViewById(R.id.textView5);
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
 
-//        button = (Button) findViewById(R.id.btn);
-//
-//        button.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent i = new Intent(getApplicationContext(),MainActivity.class);
-//                startActivity(i);
-//            }
-//        });
+        button = (Button) findViewById(R.id.btn);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(i);
+            }
+        });
 
     }
 
