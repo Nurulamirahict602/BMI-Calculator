56f33adef762ca9fb4dec88116e3d38774a199c0
Amirah <2019311667@student.uitm.edu.my>
2021-06-12 15:06:44 +0800

    bmi calculator

--- app/src/main/java/com/example/bmicalculator/MainActivity.java
+++ app/src/main/java/com/example/bmicalculator/MainActivity.java
@@ -4,9 +4,12 @@ package com.example.bmicalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -66,6 +69,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         });
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menubutton, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.bmi) {
+            Intent intent = new Intent(MainActivity.this, MainActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        else if (id == R.id.profile) {
+            Intent intent = new Intent(MainActivity.this, profile.class);
+            startActivity(intent);
+            return true;
+        } else
+            return super.onOptionsItemSelected(item);
     }
 
     //Calculate BMI
--- app/src/main/java/com/example/bmicalculator/profile.java
+++ app/src/main/java/com/example/bmicalculator/profile.java
@@ -2,6 +2,8 @@ package com.example.bmicalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.content.Intent;
 import android.view.View;
@@ -9,26 +11,50 @@ import android.text.method.LinkMovementMethod;
 import android.widget.TextView;
 
 public class profile extends AppCompatActivity {
-    public Button button;
-    TextView textView;
+//    public Button button;
+//    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
 
-        textView = findViewById(R.id.textView5);
-        textView.setMovementMethod(LinkMovementMethod.getInstance());
+//        textView = findViewById(R.id.textView5);
+//        textView.setMovementMethod(LinkMovementMethod.getInstance());
 
-        button = (Button) findViewById(R.id.btn);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(),MainActivity.class);
-                startActivity(i);
-            }
-        });
+//        button = (Button) findViewById(R.id.btn);
+//
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent i = new Intent(getApplicationContext(),MainActivity.class);
+//                startActivity(i);
+//            }
+//        });
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menubutton, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.bmi) {
+            Intent intent = new Intent(profile.this, MainActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        else if (id == R.id.profile) {
+            Intent intent = new Intent(profile.this, profile.class);
+            startActivity(intent);
+            return true;
+        } else
+            return super.onOptionsItemSelected(item);
+    }
+
 }
